cdc6b19b430615d825d60559a0b9e49fb4972eb7
Ritesh Marane <riteshmarane28@gmail.com>
2026-08-17 16:18:05 +0530

    Add deployment configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const stationRoutes = require("./routes/stationRoutes");
 const facilityRoutes = require("./routes/facilityRoutes");
 const journeyRoutes = require("./routes/journeyRoutes");
 const crowdRoutes = require("./routes/crowdRoutes");
+const chatbotRoutes = require("./routes/chatbotRoutes");
 
 const app = express();
 
@@ -21,5 +22,6 @@ app.use("/api/stations", stationRoutes);
 app.use("/api/facilities", facilityRoutes);
 app.use("/api/journeys", journeyRoutes);
 app.use("/api/crowd", crowdRoutes);
+app.use("/api/chatbot", chatbotRoutes);
 
 module.exports = app;
